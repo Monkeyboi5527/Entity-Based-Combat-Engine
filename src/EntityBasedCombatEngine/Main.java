2f5569d76bfd50af7e5126642c51c208ef3bf4db
MonkeyBoi <monkeylaguri308@gmail.com>
2026-04-08 20:25:29 -0400

    Small tweaks

--- src/EntityBasedCombatEngine/Main.java
+++ src/EntityBasedCombatEngine/Main.java
@@ -14,8 +14,13 @@ void main() {
     // *TESTING*
     TestPlayer testPlayer = new TestPlayer("testPlayer", 100, 15, EntityType.PLAYER);
     TestAlly testAlly = new TestAlly("testAlly", 100, 15, EntityType.ALLY);
-    TestAlly testAlly2 = new TestAlly("testAlly", 100, 15, EntityType.ALLY);
+    TestAlly testAlly2 = new TestAlly("testAlly2", 100, 15, EntityType.ALLY);
     Entity testEnemy = new Entity("testEnemy", 100, 15, EntityType.ENEMY);
+    Entity testEnemy2 = new Entity("testEnemy2", 100, 15, EntityType.ENEMY);
+    Entity testEnemy3 = new Entity("testEnemy3", 100, 15, EntityType.ENEMY);
+    Entity testEnemy4 = new Entity("testEnemy4", 100, 15, EntityType.ENEMY);
+    Entity testEnemy5 = new Entity("testEnemy5", 100, 15, EntityType.ENEMY);
+    Entity testEnemy6 = new Entity("testEnemy6", 100, 15, EntityType.ENEMY);
     Entity testBoss = new Entity("testBoss", 100, 15, EntityType.BOSS);
 
     List<Entity> entities = new ArrayList<>();
@@ -23,11 +28,16 @@ void main() {
     entities.add(testAlly);
     entities.add(testAlly2);
     entities.add(testEnemy);
+    entities.add(testEnemy2);
+    entities.add(testEnemy3);
+    entities.add(testEnemy4);
+    entities.add(testEnemy5);
+    entities.add(testEnemy6);
     entities.add(testBoss);
 
 
     CombatEngine combatEngine = new CombatEngine();
-        combatEngine.startCombat(entities, scanner);
+    combatEngine.startCombat(entities, scanner, random);
 
 
 }
--- src/EntityBasedCombatEngine/combat/CombatEngine.java
+++ src/EntityBasedCombatEngine/combat/CombatEngine.java
@@ -17,12 +17,36 @@ public class CombatEngine {
     List<Entity> enemyTeam = new ArrayList<>();
     List<Entity> playerTeam = new ArrayList<>();
 
+    List<Entity> initialized = new ArrayList<>();
     TestPlayer player;
     TestAlly ally;
     Entity enemy;
     Entity boss;
 
-    Random random = new Random();
+
+
+
+
+    /**
+     *  Actually runs the combat
+     *
+     * @param entities
+     * @param scanner
+     */
+    public void startCombat(List<Entity> entities, Scanner scanner, Random random) {
+        initialize(entities);
+        do {
+            System.out.println("*TESTING* Starting Combat");
+            System.out.print("*TESTING* Printing Entities: ");
+            for (Entity entity : initialized) {
+                System.out.print(entity.getName() + " ");
+            }
+
+
+            System.out.println("*TESTING* KILLING PLAYER");
+            player.takeDamage(10000);
+        } while (combatCondition());
+    }
 
     /**
      * Function -> To check if the Player team or the Enemy team is alive.
@@ -30,7 +54,6 @@ public class CombatEngine {
      *     Details: The loop goes through each {@link Entity} and sorts them into teams.
      *     Player team consists of {@link EntityType#PLAYER} and {@link EntityType#ALLY} while Enemy {@link EntityType#ENEMY} and or {@link EntityType#BOSS},
      *     Then checks if whether each entity in the team is alive or not.
-     *
      * */
     public boolean combatCondition() {
         int teamSize;
@@ -40,45 +63,30 @@ public class CombatEngine {
             return false;
         }
 
-         // Checks each entity on enemy team
-         for (Entity enemyTeamEntity : enemyTeam) {
-             teamSize = enemyTeam.size();
-             System.out.println("*TESTING* " + "Counter(after isAlive check(enemy)): " + counter);
-             System.out.println("*TESTING* " + "Checking: " + enemyTeamEntity.getName());
-             if (!enemyTeamEntity.isAlive()){
-                 counter++;
-                 System.out.println("*TESTING* " + "Counter(after isAlive check(enemy)): " + counter);
-                 if (counter == teamSize){
-                     System.out.println("ENEMY TEAM DEAD");
-                     return false;
-                 }
-             }
-         }
+        // Checks each entity on enemy team
+        for (Entity enemyTeamEntity : enemyTeam) {
+            teamSize = enemyTeam.size();
+            System.out.println("*TESTING* " + "Counter(after isAlive check(enemy)): " + counter);
+            System.out.println("*TESTING* " + "Checking: " + enemyTeamEntity.getName());
+            if (!enemyTeamEntity.isAlive()){
+                counter++;
+                System.out.println("*TESTING* " + "Counter(after isAlive check(enemy)): " + counter);
+                if (counter == teamSize){
+                    System.out.println("ENEMY TEAM DEAD");
+                    return false;
+                }
+            }
+        }
 
         return true;
     }
 
-
     /**
-     *  Actually runs the combat
+     *  Choices for the player
      *
-     * @param entities
      * @param scanner
      */
-    public void startCombat(List<Entity> entities, Scanner scanner) {
-        initialize(entities);
-        do {
-            System.out.println("*TESTING* Starting Combat");
-
-
-            System.out.println("*TESTING* KILLING PLAYER");
-            player.takeDamage(10000);
-
-
-        } while (combatCondition());
-    }
-
-    public void choice(Scanner scanner) {
+    private void choice(Scanner scanner) {
         System.out.println("Player " + player.getName()  + "'s turn");
         System.out.println("1. Attack");
         System.out.println("2. Heal(2 Uses Max)");
@@ -112,8 +120,13 @@ public class CombatEngine {
             }
         }
 
-
-    public void action(Entity entity, Random random) {
+    /**
+     *  A Random int decides if to attack or to heal
+     *
+     * @param entity
+     * @param random
+     */
+    private void action(Entity entity, Random random) {
         System.out.println("Ally " + ally.getName() + "'s turn");
         if (entity.getEntityType() == EntityType.ALLY) {
            switch (random.nextInt(1, 3)) {
@@ -138,33 +151,43 @@ public class CombatEngine {
         }
     }
 
-    public void initialize(List<Entity> entities) {
+    /**
+     *  Actually runs the combat
+     *
+     * @param entities
+     */
+    public List<Entity> initialize(List<Entity> entities) {
         for (Entity entity : entities) {
             switch (entity.getEntityType()) {
                 case PLAYER -> {
                     player = new TestPlayer(entity.getName(), entity.getHealth(), entity.getAttackDamage(), EntityType.PLAYER);
                     System.out.println("*TESTING* " + entity.getName() + " added!");
+                    initialized.add(player);
                     playerTeam.add(player);
                 }
                 case ALLY -> {
                     ally = new TestAlly(entity.getName(), entity.getHealth(), entity.getAttackDamage(), EntityType.ALLY);
                     System.out.println("*TESTING* " + entity.getName() + " added!");
+                    initialized.add(ally);
                     playerTeam.add(ally);
                 }
                 case ENEMY -> {
                     enemy = new Entity(entity.getName(), entity.getHealth(), entity.getAttackDamage(), EntityType.ENEMY);
                     System.out.println("*TESTING* " + entity.getName() + " added!");
+                    initialized.add(entity);
                     enemyTeam.add(enemy);
                 }
                 case BOSS -> {
                     boss = new Entity(entity.getName(), entity.getHealth(), entity.getAttackDamage(), EntityType.BOSS);
                     System.out.println("*TESTING* " + entity.getName() + " added!");
+                    initialized.add(boss);
                     enemyTeam.add(boss);
                 }
                 default -> System.out.println("*TESTING* SOMETHING WENT WRONG");
             }
         }
         System.out.println("*TESTING* Sorting complete! PlayerTeam: " + playerTeam.size() + " | EnemyTeam: " + enemyTeam.size());
+        return initialized;
     }
 }
 
