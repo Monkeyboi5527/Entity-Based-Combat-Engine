a8d17b2d4b8b70ca3d0d59ebc38d3b488772a839
MonkeyBoi <monkeylaguri308@gmail.com>
2026-04-29 22:22:32 -0400

    Multiple Entities testing 3

--- src/EntityBasedCombatEngine/Main.java
+++ src/EntityBasedCombatEngine/Main.java
@@ -15,25 +15,25 @@ void main() {
     // *TESTING*
     TestPlayer testPlayer = new TestPlayer("testPlayer", 100000, 15, EntityType.PLAYER);
     TestAlly testAlly = new TestAlly("testAlly", 80, 10, EntityType.ALLY);
-    TestAlly testAlly2 = new TestAlly("testAlly2", 100, 15, EntityType.ALLY);
+//    TestAlly testAlly2 = new TestAlly("testAlly2", 100, 15, EntityType.ALLY);
     TestEnemy testEnemy = new TestEnemy("testEnemy", 80, 10, EntityType.ENEMY);
-    TestEnemy testEnemy2 = new TestEnemy("testEnemy2", 80, 10, EntityType.ENEMY);
-    TestEnemy testEnemy3 = new TestEnemy("testEnemy3", 80, 10, EntityType.ENEMY);
-    TestEnemy testEnemy4 = new TestEnemy("testEnemy4", 80, 10, EntityType.ENEMY);
-    TestEnemy testEnemy5 = new TestEnemy("testEnemy5", 80, 10, EntityType.ENEMY);
-    TestEnemy testEnemy6 = new TestEnemy("testEnemy6", 80, 10, EntityType.ENEMY);
+//    TestEnemy testEnemy2 = new TestEnemy("testEnemy2", 80, 10, EntityType.ENEMY);
+//    TestEnemy testEnemy3 = new TestEnemy("testEnemy3", 80, 10, EntityType.ENEMY);
+//    TestEnemy testEnemy4 = new TestEnemy("testEnemy4", 80, 10, EntityType.ENEMY);
+//    TestEnemy testEnemy5 = new TestEnemy("testEnemy5", 80, 10, EntityType.ENEMY);
+//    TestEnemy testEnemy6 = new TestEnemy("testEnemy6", 80, 10, EntityType.ENEMY);
     TestBoss testBoss = new TestBoss("testBoss", 150, 15, EntityType.BOSS);
 
     List<Entity> entities = new ArrayList<>();
     entities.add(testPlayer);
     entities.add(testAlly);
-    entities.add(testAlly2);
+//    entities.add(testAlly2);
     entities.add(testEnemy);
-    entities.add(testEnemy2);
-    entities.add(testEnemy3);
-    entities.add(testEnemy4);
-    entities.add(testEnemy5);
-    entities.add(testEnemy6);
+//    entities.add(testEnemy2);
+//    entities.add(testEnemy3);
+//    entities.add(testEnemy4);
+//    entities.add(testEnemy5);
+//    entities.add(testEnemy6);
     entities.add(testBoss);
 
 
